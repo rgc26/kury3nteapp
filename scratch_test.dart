4346b8e6fac89c1fb1148063dfa7c0dc447016c4
RM <romark@gmail.com>
2026-05-08 08:58:33 +0800

    chore: cleanup unused files and remove node_modules from git

--- scratch_test.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-
-void main() async {
-  final query = 'Barangay 73, Caloocan';
-  final url = Uri.parse('https://nominatim.openstreetmap.org/search?q=$query&format=json');
-  final response = await http.get(url, headers: {'User-Agent': 'kury3nte/1.0'});
-  print('Response for $query: ${response.body}');
-}
